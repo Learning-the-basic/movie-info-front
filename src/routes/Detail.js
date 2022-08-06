d4d38613bc96c521acd9aedca43239ee43b4f764
MODIFYC <chsj5742@naver.com>
2022-08-06 23:39:39 +0900

    상세 검색 화면1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Detail() {
-    return (
-        <div>
-            <h1>Detail</h1>
-        </div>
-        
-    )
-}
+  const tname = useParams();
+//   console.log(tname)
+  const KEY2 = "80HF21BI401E15RFQ193"    //KMDB
+//   const DATE = moment().subtract(1, 'day').format('YYYYMMDD');
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&title=${tname.id}&ServiceKey=${KEY2}`)
+    ).json();
+    return {
+        // "title": movie.movieNm,
+        // "rnum": movie.rnum,
+        // "rank": movie.rank,
+        // "openDt": movie.openDt,
+        // "audiCnt": movie.audiCnt,
+        // "audiAcc": movie.audiCnt,
+        // "posters":json.Data[0].Result[0].posters,
+        // "kmdbURL":json.Data[0].Result[0].kmdbUrl
+    }
 
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <h1>Detail</h1>
+    )
+  
+
+}
 export default Detail;
